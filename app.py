7bdff2cd619654f99c8209d6de5cf164b4500520
nathaly lopess <lopesnathaly1810@gmail.com>
2024-12-07 13:34:20 -0300

    Adicionando flash na tela de cadastro

--- app.py
+++ app.py
@@ -44,15 +44,40 @@ def register():
     if request.method == 'POST':
         email = request.form['email']
         senha = request.form['pass']
+        
+        # Verifica se ambos os campos estão preenchidos
+        if not email or not senha:
+            flash("Por favor, preencha todos os campos.")  # Mensagem de erro
+            return redirect(url_for('register')) 
+        
+        # Verifica se o email já está cadastrado
+        conexao = obter_conexao()
+        SELECT = 'SELECT * FROM usuarios WHERE email=?'
+        user_exists = conexao.execute(SELECT, (email,)).fetchone()
+        conexao.close()
+        
+        if user_exists:
+            flash("Este email já está cadastrado.")  # Mensagem de erro
+            return redirect(url_for('register'))  # Redireciona de volta para a página de registro
+        
+        # Insere o novo usuário no banco de dados
         conexao = obter_conexao()
         INSERT = 'INSERT INTO usuarios(email,senha) VALUES (?,?)'
         conexao.execute(INSERT, (email, senha))
         conexao.commit()
         conexao.close()
-        return redirect(url_for('index'))
+
+        # Recupera o usuário recém-criado
+        user = User.get_by_email(email)
+
+        # Faz login automático após o registro
+        if user:
+            login_user(user)
+
+        # Redireciona para o dashboard
+        return redirect(url_for('dash'))
 
     return render_template('register.html')
-
 
 @app.route('/dash')
 @login_required
